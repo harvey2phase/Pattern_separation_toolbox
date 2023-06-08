4329c90da29b75b3da8f8ad319fe8b2e9ce1611e
Harvey Wang <harvey@dal.ca>
2023-06-08 13:21:25 -0300

    Reformat/cleanup

--- run_data.m
+++ run_data.m
@@ -1,24 +1,24 @@
 start_pattern_separation;
 
-n_p11s = 10;
-n_trials = 10;
 eps = 10^-2;
 e1s = [0.10, 0.20, 0.30, 0.40, 0.50];
 
 p110_start_index = 1;
+n_p11s = 10;
 trial_start_index = 1;
+n_trials = 10;
 
 data_folder_name = "spiketimes";
-rr_folder = "results/rr/";
+results_folder = "results/rr/";
 
 for i = 1:length(e1s)
     e1_folder = data_folder_name + "/e1=" + compose("%0.2f", e1s(i));
-    p110s = linspace(eps, e1s(i)-eps, n_p11s);
-    rr_results = cell(n_p11s, n_trials);
+    p11s = linspace(eps, e1s(i)-eps, n_p11s);
+    results = cell(n_p11s, n_trials);
 
     for j=p110_start_index:n_p11s
         
-        p110_folder = e1_folder + "/" + "p11=" + compose("%0.2f", p110s(j));
+        p110_folder = e1_folder + "/" + "p11=" + compose("%0.2f", p11s(j));
         disp(p110_folder);
 
         for k=trial_start_index:n_trials
@@ -32,12 +32,13 @@ for i = 1:length(e1s)
 
             fprintf("%f, ", estRR_obj.RR);
 
-            rr_results{j, k} = estRR_obj.RR;
+            results{j, k} = estRR_obj.RR;
         end
+
         disp("writing...");
-        rr_e1_folder = rr_folder + "e1=" + string(e1s(i)) + ".csv";
-        disp(rr_e1_folder);
-        csvwrite(rr_e1_folder, rr_results);
+        e1_results_folder = results_folder + "e1=" + string(e1s(i)) + ".csv";
+        disp(e1_results_folder);
+        csvwrite(e1_results_folder, results);
     end
 end
 
