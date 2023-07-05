51dafd31a00353ecf880012ff8cd9ad2daa209fc
Harvey Wang <harvey@dal.ca>
2023-07-05 16:04:16 -0300

    Added options

--- run_e2_data.m
+++ run_e2_data.m
@@ -1,5 +1,7 @@
 start_pattern_separation;
 
+compute_mi = false;
+compute_te = true;
 
 n_e2s = 10;
 min_e2 = 0.1;
@@ -10,13 +12,22 @@ trial_start_index = 1;
 n_trials = 10;
 
 data_folder_name = "spiketimes/variable-e2/e1=0.30";
-%mi_folder = "results/mi/";
-%rr_folder = "results/rr/";
-te_folder = "results/te/";
 
-%mi_results = cell(n_trials);
-%rr_results = cell(n_trials);
-te_results = cell(n_trials);
+if compute_mi
+    mi_folder = "results/mi/";
+    rr_folder = "results/rr/";
+end
+if compute_te
+    te_folder = "results/te/";
+end
+
+if compute_mi
+    mi_results = cell(n_trials);
+    rr_results = cell(n_trials);
+end
+if compute_te
+    te_results = cell(n_trials);
+end
 
 for j=1:n_e2s
         
@@ -26,32 +37,40 @@ for j=1:n_e2s
     for k=trial_start_index:n_trials
         infile_name = data_folder_name + "/in_times" + string(k) + ".csv";
         outfile_name = e2_folder + "/out_times" + string(k) + ".csv";
-        disp(infile_name);
-        disp(outfile_name);
+        %disp(infile_name);
+        %disp(outfile_name);
 
         in_times = read_spiketimes(infile_name);
         out_times = read_spiketimes(outfile_name);
 
-        %patsep = analyse_pattern_separation(in_times,out_times,'estimate',true);
+        if compute_mi
+            patsep = analyse_pattern_separation(in_times,out_times, 'estimate', true);
 
-        %mi_results{j, k} = patsep.info_details.MI;
-        %rr_results{j, k} = patsep.info_details.RR;
-        %fprintf("(%f, %f); ", patsep.info_details.MI, patsep.info_details.RR);
-            
-        num_params = struct;
-        TEoptions = '-max_bins -max_code -par';
-        TE_obj = TE_function(in_times,out_times,[],num_params,TEoptions);
-        te_results{j, k} = TE_obj.TE;
-        fprintf("%f, ", TE_obj.TE);
+            mi_results{j, k} = patsep.info_details.MI;
+            rr_results{j, k} = patsep.info_details.RR;
+            fprintf("(%f, %f); ", patsep.info_details.MI, patsep.info_details.RR);
+        end
+        
+        if compute_te
+            num_params = struct;
+            TEoptions = '-max_bins -max_code -par';
+            TE_obj = TE_function(in_times, out_times, [], num_params, TEoptions);
+            te_results{j, k} = TE_obj.TE;
+            fprintf("%f, ", TE_obj.TE);
+        end
         
     end
 
     disp("writing...");
     e2_results_folder = "variable-e2-e1=0.3.csv";
 
-    %csvwrite(mi_folder + e2_results_folder, mi_results);
-    %csvwrite(rr_folder + e2_results_folder, rr_results);
-    csvwrite(te_folder + e1_folder, te_results);
+    if compute_mi
+        csvwrite(mi_folder + e2_results_folder, mi_results);
+        csvwrite(rr_folder + e2_results_folder, rr_results);
+    end
+    if compute_te
+        csvwrite(te_folder + e1_folder, te_results);
+    end
 
 end
 
